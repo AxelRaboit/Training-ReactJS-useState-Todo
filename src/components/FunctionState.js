22b78a8f179c3317e817d1022e6eeece107f27ce
Axel Raboit <axel.raboit@gmail.com>
2022-02-01 17:47:14 +0100

    add two component, one with the state in a class the other with the state in a function

---
+++ src/components/FunctionState.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+const FunctionState = () => {
+
+    const [counter, setCounter] = useState(0);
+
+    const addOne = () => {
+        setCounter(counter + 1)
+    }
+
+    return (
+        <div>
+            <p>Function State: {counter}</p>
+            <button onClick={addOne}>State dans Function</button>
+        </div>
+    )
+}
+
+export default FunctionState;
